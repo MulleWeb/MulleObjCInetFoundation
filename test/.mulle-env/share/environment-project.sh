1087b1046c478bed87f7d6f6c0e9fd841c14062f
Nat! <nat@codeon.de>
2018-12-21 15:25:46 +0100

    fix travis

---
+++ test/.mulle-env/share/environment-project.sh
@@ -0,0 +1,48 @@
+#
+#
+#
+export PROJECT_NAME="MulleObjCInetFoundation"
+
+
+#
+#
+#
+export PROJECT_IDENTIFIER="MulleObjCInetFoundation"
+
+
+#
+#
+#
+export PROJECT_DOWNCASE_IDENTIFIER="mulle_objc_inet_foundation"
+
+
+#
+#
+#
+export PROJECT_UPCASE_IDENTIFIER="MULLE_OBJC_INET_FOUNDATION"
+
+
+#
+#
+#
+export PROJECT_TYPE="none"
+
+
+#
+#
+#
+export PROJECT_LANGUAGE="c"
+
+
+#
+#
+#
+export PROJECT_DIALECT="objc"
+
+
+#
+#
+#
+export PROJECT_EXTENSIONS="m:aam"
+
+
